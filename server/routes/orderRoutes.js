bf8d58849ebf42dce9687f2bc0a07eae9566d978
Sankalp Kanubhai Sachapara <101259898+Sankalp-Sachapara@users.noreply.github.com>
2025-03-27 08:39:18 -0400

    Add order routes

---
+++ server/routes/orderRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  updateOrderStatus,
+  getMyOrders,
+  getOrders,
+  deleteOrder,
+} = require('../controllers/orderController');
+const { protect, admin } = require('../middleware/authMiddleware');
+
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
+router.route('/myorders').get(protect, getMyOrders);
+router.route('/:id').get(protect, getOrderById).delete(protect, admin, deleteOrder);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
+router.route('/:id/status').put(protect, admin, updateOrderStatus);
+
+module.exports = router;
